47d846b7ca7b7a42b95ad9283feecb14b38a3c02
Bruno Gonçalves <bpvgoncalves@users.noreply.github.com>
2023-02-03 15:04:48 +0100

    change vault_init and vault_manager internals

--- R/vault.R
+++ R/vault.R
@@ -19,8 +19,8 @@
 #'
 #' Initialize new vault
 #'
-#' @param vault_path   Path to vault folder
-#' @param key_size     Size for RSA private key (Default: 8192)
+#' @param vault_path   Path to vault file
+#' @param key_size     Size for RSA key (Default: 8192 bits)
 #'
 #' @return NULL
 
@@ -28,48 +28,53 @@
 vault_init <- function(vault_path = NULL, key_size = 8192) {
   cat("Initializing Vault DB\n")
 
-  if (is.null(vault_path)) stop("Invalid vault path.")
-  if (!file.exists(vault_path)) {
-    db <- DBI::dbConnect(RSQLite::SQLite(), vault_path)
-    DBI::dbExecute(db,
-                   "CREATE TABLE metadata (
+  if (is.null(vault_path))
+    stop("Invalid vault path.")
+  if (file.exists(vault_path))
+    stop("File already exists. Try open a Vault Manager or change the path.")
+
+  pass <- argon2::blake2b(openssl::askpass("Please enter vault password:"))
+  if (!identical(pass,
+                 argon2::blake2b(openssl::askpass("Please confirm vault password:")))) {
+    stop("Passwords do not match!")
+  }
+
+  cat("Deriving symmetric keys and generating asymmetric key pair. Please be patient!\n")
+  k <- keygen_full(pass, key_size)
+  rm(pass)
+  gc()
+
+  db <- DBI::dbConnect(RSQLCipher::SQLite(), vault_path, key = k$keyDB$raw_hash)
+
+  DBI::dbExecute(db,
+                 "CREATE TABLE metadata (
                    md_key TEXT NOT NULL,
                    md_value TEXT NOT NULL,
                    CONSTRAINT metadata_PK PRIMARY KEY (md_key));")
-    DBI::dbExecute(db,
-                   "CREATE TABLE item (
+  DBI::dbExecute(db,
+                 "CREATE TABLE item (
                    item_id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
                    item_title TEXT NOT NULL,
                    item_type TEXT NOT NULL,
                    item BLOB NOT NULL);")
-  } else {
-    stop("Path already exists. Try open a Vault Manager or change the path.")
-  }
 
-  cat("Deriving key from password. Please be patient!\n")
-  key <- argon2::argon2_kdf(openssl::askpass("Please enter vault password:"))
-  if (!identical(key,
-                 argon2::argon2_kdf(openssl::askpass("Please confirm vault password:"),
-                                    key$salt))) {
-    stop("Passwords do not match!")
-  }
+  q <- DBI::dbSendStatement(db,
+                            "INSERT INTO metadata (md_key, md_value) VALUES (:k, :v);")
 
-  q <- DBI::dbSendStatement(db, "INSERT INTO metadata (md_key, md_value) VALUES (:k, :v);")
   DBI::dbBind(q, params = list(k = "salt",
-                               v = openssl::base64_encode(key$salt)))
+                               v = openssl::base64_encode(k$keyRSA$salt)))
 
-  cat("Creating keypair. This may take a while...\n")
-  rsakey <- openssl::rsa_keygen(key_size)
   DBI::dbBind(q, params = list(k = "prv",
-                               v = openssl::write_pem(rsakey,
+                               v = openssl::write_pem(k$RSA,
                                                       NULL,
-                                                      password = paste0(key$key, collapse = ""))))
+                                                      password = paste0(k$keyRSA$key,
+                                                                        collapse = ""))))
   DBI::dbBind(q, params = list(k = "pub",
-                               v = openssl::write_pem(rsakey$pubkey,
+                               v = openssl::write_pem(k$RSA$pubkey,
                                                       NULL)))
   DBI::dbClearResult(q)
   DBI::dbDisconnect(db)
-  rm(rsakey, key, db, q)
+  rm(k, db, q)
   return(0)
 }
 
@@ -77,22 +82,26 @@ vault_init <- function(vault_path = NULL, key_size = 8192) {
 
 #' Vault Manager   `r lifecycle::badge('experimental')`
 #'
-#' Manages the safe vault folder
+#' Manages the safe vault file
 #'
-#' @param vault_path  Path to vault folder
+#' @param vault_path  Path to vault file
+#' @param strict      Boolean. If TRUE the password will be requested every time an item is read
+#'  from the vault.
 #'
 #' @return
 #' Vault Manager enclosure
 #'
 #' @export
-vault_manager <- function(vault_path = NULL) {
+vault_manager <- function(vault_path = NULL, strict = FALSE) {
 
   if (is.null(vault_path)) stop("Invalid vault path.")
 
+  p <- argon2::blake2b(openssl::askpass("Please enter vault password:"))
+  k <- keygen_keyDBonly(p)
+
+  db <- DBI::dbConnect(RSQLCipher::SQLite(), vault_path, key = k$keyDB$raw_hash)
+
   keys <- new.env(TRUE, emptyenv())
-
-  db <- DBI::dbConnect(RSQLite::SQLite(), vault_path)
-
   keys$salt <- openssl::base64_decode(DBI::dbGetQuery(db,
                                                       "SELECT md_value
                                                       FROM metadata
@@ -101,6 +110,10 @@ vault_manager <- function(vault_path = NULL) {
                                                        "SELECT md_value
                                                        FROM metadata
                                                        WHERE md_key='pub'")[[1]])
+  if(!strict)
+    keys$prv_key_k <- keygen_keyRSAonly(p, keys$salt)[[1]][[1]]
+  else
+    keys$prv_key_k <- NULL
 
 
   read_vault <- function () {
@@ -114,6 +127,57 @@ vault_manager <- function(vault_path = NULL) {
     DBI::dbDisconnect(db)
   }
 
+  change_key <- function() {
+
+    op <- argon2::blake2b(openssl::askpass("Please enter OLD vault password:"))
+    np <- argon2::blake2b(openssl::askpass("Please enter NEW vault password:"))
+    if (!identical(np,
+                   argon2::blake2b(openssl::askpass("Please confirm NEW vault password:")))) {
+      stop("Passwords do not match!")
+    }
+
+    cat("Deriving new keys from password. Please be patient!\n")
+    ok1 <- keygen_keyDBonly(op)[[1]][[1]]
+    nk1 <- keygen_keyDBonly(np)[[1]][[1]]
+
+    if (is.null(keys$prv_key_k)) {
+      ok2 <- keygen_keyRSAonly(op, keys$salt)[[1]]
+    } else {
+      ok2 <- keys$prv_key_k
+    }
+    nk2 <- keygen_keyRSAonly(np, 32L)[[1]]
+    rm(op, np)
+    gc()
+
+    # Change file encryption key
+    RSQLCipher::dbChangeKey(db, ok1, nk1)
+    rm(ok1, nk1)
+    gc()
+
+    # Change RSA key encryption
+    q <- DBI::dbSendStatement(db,
+                              "UPDATE metadata SET md_value=:v WHERE md_key=:k;")
+
+    DBI::dbBind(q, params = list(k = "salt",
+                                 v = openssl::base64_encode(nk2$salt)))
+    keys$salt <- nk2$salt
+
+    v <- openssl::write_pem(openssl::read_key(DBI::dbGetQuery(db,
+                                                              "SELECT md_value
+                                                              FROM metadata
+                                                              WHERE md_key='prv'")[[1]],
+                                              paste0(ok2, collapse = "")),
+                            NULL,
+                            password = paste0(nk2$key, collapse = ""))
+    DBI::dbBind(q, params = list(k = "prv",
+                                 v = v))
+    if(!strict)
+      keys$prv_key_k <- nk2$key
+    rm(ok2, nk2)
+    gc()
+
+  }
+
   item_store <- function(item) {
     name <- item$title
     type <- class(item)[1]
@@ -125,7 +189,7 @@ vault_manager <- function(vault_path = NULL) {
                               VALUES
                               (:ttl, :type, :item)")
     df <- data.frame(item = I(serialize(item, NULL)))
-    DBI::dbBind(q, params = list(ttl = "name",
+    DBI::dbBind(q, params = list(ttl = name,
                                  type = type,
                                  item = df))
     DBI::dbClearResult(q)
@@ -135,14 +199,21 @@ vault_manager <- function(vault_path = NULL) {
 
   item_read <- function(name) {
 
-    kdf <- argon2::argon2_kdf(openssl::askpass("Please enter vault password:"),
-                              keys$salt)
     q <- DBI::dbSendQuery(db,
                           "SELECT item
                           FROM item
                           WHERE item_title = :ttl;")
     DBI::dbBind(q, params = list(ttl = name))
     item <- unserialize(DBI::dbFetch(q)$item[[1]])
+    DBI::dbClearResult(q)
+
+    if (is.null(keys$prv_key_k)) {
+      k <- keygen_keyRSAonly(argon2::blake2b(openssl::askpass("Please enter vault password:")),
+                             keys$salt)
+    } else {
+      k <- keys$prv_key_k
+    }
+
     item <- openssl::decrypt_envelope(item$data,
                                       item$iv,
                                       item$session,
@@ -150,15 +221,15 @@ vault_manager <- function(vault_path = NULL) {
                                                       "SELECT md_value
                                                        FROM metadata
                                                        WHERE md_key='prv'")[[1]],
-                                      password = paste0(kdf$key, collapse = ""))
-    DBI::dbClearResult(q)
-    rm(q, kdf)
+                                      password = paste0(k, collapse = ""))
+    rm(q, k)
     return(unserialize(item))
   }
 
 
   return(list(read_vault = read_vault,
               close_vault = close_vault,
+              # change_key = change_key,
               item_store = item_store,
               item_read = item_read))
 
